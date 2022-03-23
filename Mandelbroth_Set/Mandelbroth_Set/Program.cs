f3cd31c082b4992f9adaecbfe50c0451d5f71ff9
Masood <57627236+masxxiii@users.noreply.github.com>
2022-03-23 21:07:06 +0700

    Update Program.cs

--- Mandelbroth_Set/Mandelbroth_Set/Program.cs
+++ Mandelbroth_Set/Mandelbroth_Set/Program.cs
@@ -6,18 +6,5 @@ using System.Windows.Forms;
 
 namespace Mandelbroth_Set
 {
-    static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
+
 }
